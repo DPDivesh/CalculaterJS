780e2304350bd7ba6833fcb12101ad4c84a195f5
DPDivesh <diveshpatel39@gmail.com>
2020-12-15 23:55:41 -0800

    Fixed Html5 and Css setup and started JS

---
+++ app.js
@@ -0,0 +1,27 @@
+let displayMath = document.querySelector(".results")
+
+console.log(displayMath);
+
+let numbersInputed = [];
+let inputResult = [];
+
+//Doesn't work only takes one input 
+// function numbersToMath(number){
+//   console.log(number);
+// if(numbersInputed.length != 3 ){
+//   numbersInputed.push(number);
+// }
+// if(numbersInputed.length == 3){
+//   if(numbersInputed[2]= "+"){
+//     addition();
+//     console.log(numbersInputed);
+//   }
+// }
+// }
+
+
+function addition(){
+
+
+
+}
